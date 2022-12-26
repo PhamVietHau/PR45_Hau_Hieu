80980414c41edb14f44e08fb3a2538ec4922943f
PhamVietHau <101783911+PhamVietHau@users.noreply.github.com>
2022-12-26 18:10:01 +0700

    entity

---
+++ PR45_Hau_Hieu/src/main/java/entity/PaymentMethod.java
@@ -0,0 +1,13 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class PaymentMethod {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+}
