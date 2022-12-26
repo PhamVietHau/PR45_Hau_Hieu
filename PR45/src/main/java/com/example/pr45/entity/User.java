1760dea3c0a1efc64530ff05fa63da460cc20654
huynhhieultkck <huynhhieultkck@live.com>
2022-12-26 21:38:27 +0700

    no message

--- PR45/src/main/java/com/example/pr45/entity/User.java
+++ PR45/src/main/java/com/example/pr45/entity/User.java
@@ -1,6 +1,15 @@
 package com.example.pr45.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "amount")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
     private int ProductId;
     private int ColorId;
+    private int SizeId;
+    private int Amount;
 }
